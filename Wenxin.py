926c86693727b2d33e3762eef211aefc7dcc9e8d
LoffiBubby <lwk1609964274@outlook.com>
2022-10-19 22:45:51 +1100

    change a few pictures and post on the Wall

--- Wenxin.py
+++ Wenxin.py
@@ -4,8 +4,8 @@ from wenxin_api.tasks.text_to_image import TextToImage
 wenxin_api.ak = "HL2IUa0gDfGKSAHAG6O2GGG2G3WbEMCv"
 wenxin_api.sk = "4QOKIOm5G9CICZ5udFiLHmpacLosV2GG"
 input_dict = {
-    "text": "大都市",
-    "style": "蒸汽波艺术",
+    "text": "极乐迪斯科，猫，死亡",
+    "style": "赛博朋克",
     "resolution": "1024*1024"
 }
 rst = TextToImage.create(**input_dict)
