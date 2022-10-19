914926d319f0dbfd0acc6429ec1cc6d0d4f0c503
LoffiBubby <lwk1609964274@outlook.com>
2022-10-19 12:09:59 +1100

    some new changes

---
+++ [Base]BaseChip_pipo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="[Base]BaseChip_pipo" tilewidth="32" tileheight="32" tilecount="1064" columns="8">
+ <image source="../../../Downloads/Pipoya RPG Tileset 32x32/[Base]BaseChip_pipo.png" width="256" height="4256"/>
+</tileset>
